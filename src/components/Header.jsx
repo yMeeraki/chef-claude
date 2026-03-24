ceedf184357be43e6dd7b44af34b9fcedc03f402
Yukti Gupta <yuktigupta95@gmail.com>
2026-03-24 21:05:56 +0530

    updated layout of code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,10 @@ function Header() {
     <>
       <header className="header">
         <div className="header-container">
-          <img src="https://media.istockphoto.com/id/1166149111/vector/chef-in-a-cooking-hat-vector-outline-icon-food-concept-for-graphic-design-logo-web-site.jpg?s=612x612&w=0&k=20&c=ars2r_Y6s5OSSLHymV6MAvhxokUDqxSaxDK36ibhjgk=" alt="chef-claude-icon" />
+          <img
+            src="https://media.istockphoto.com/id/1166149111/vector/chef-in-a-cooking-hat-vector-outline-icon-food-concept-for-graphic-design-logo-web-site.jpg?s=612x612&w=0&k=20&c=ars2r_Y6s5OSSLHymV6MAvhxokUDqxSaxDK36ibhjgk="
+            alt="chef-claude-icon"
+          />
           <p>Chef Claude</p>
         </div>
       </header>
