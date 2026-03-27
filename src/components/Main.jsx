8ef9e95d1ab926d0a999bc8162b73033731e7395
Yukti Gupta <yuktigupta95@gmail.com>
2026-03-27 13:49:05 +0530

    feat:conditional rendering

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,43 @@
+import React from "react";
+
 export default function Main() {
+  const [ingredients, setIngredients] = React.useState([]);
+
+  const ingredientsListItems = ingredients.map((ingredient) => (
+    <li key={ingredient}>{ingredient}</li>
+  ));
+
+  function addIngredient(formData) {
+    const newIngredient = formData.get("ingredient");
+    setIngredients((prevIngredients) => [...prevIngredients, newIngredient]);
+  }
+
   return (
     <main>
-      <form className="add-ingredient-form">
+      <form action={addIngredient} className="add-ingredient-form">
         <input
           type="text"
           placeholder="e.g. oregano"
           aria-label="Add ingredient"
+          name="ingredient"
         />
         <button>Add ingredient</button>
       </form>
+      {ingredients.length > 0 && (
+        <section>
+          <h2>Ingredients on hand:</h2>
+          <ul className="ingredients-list" aria-live="polite">
+            {ingredientsListItems}
+          </ul>
+          <div className="get-recipe-container">
+            <div>
+              <h3>Ready for a recipe?</h3>
+              <p>Generate a recipe from your list of ingredients.</p>
+            </div>
+            <button>Get a recipe</button>
+          </div>
+        </section>
+      )}
     </main>
   );
 }
