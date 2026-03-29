82bec7c327add6dc6cbfff06154baa3db35a08aa
Yukti Gupta <yuktigupta95@gmail.com>
2026-03-29 20:53:49 +0530

    feat:footer to App()

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import Header from "./components/Header.jsx";
 import Main from "./components/Main.jsx";
 
 function App() {
+  const date = new Date()
   return (
     <>
       <Header />
       <Main />
+      <footer>copy; {date.getFullYear()}</footer>
     </>
   );
 }
