5aab3088681fa834dd75fd5716fa01dcbe35c1a8
Yukti Gupta <yuktigupta95@gmail.com>
2026-04-02 14:49:37 +0530

    refactor: move recipe section to ClaudeRecipe and ingredients list to IngredientsList component

--- src/components/IngredientsList.jsx
+++ src/components/IngredientsList.jsx
@@ -1,5 +1,21 @@
 function IngredientsList(props) {
-  return <li>{props.ingredient}</li>;
+  return (
+    <section>
+      <h2>Ingredients on hand:</h2>
+      <ul className="ingredients-list" aria-live="polite">
+        {props.ingredientsListItems}
+      </ul>
+      {props.ingredients.length > 3 && (
+        <div className="get-recipe-container">
+          <div>
+            <h3>Ready for a recipe?</h3>
+            <p>Generate a recipe from your list of ingredients.</p>
+          </div>
+          <button onClick={props.getRecipe}>Get a recipe</button>
+        </div>
+      )}
+    </section>
+  );
 }
 
 export default IngredientsList;
